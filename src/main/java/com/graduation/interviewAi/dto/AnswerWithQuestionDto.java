d455ab24584f18e838d5c00624cf5aec8568ff28
jinhee0 <jinhee8784@naver.com>
2025-04-05 14:53:10 +0900

    feat: #5 인터뷰ê¸ID기반 질문-답변 조회

--- src/main/java/com/graduation/interviewAi/dto/AnswerWithQuestionDto.java
+++ src/main/java/com/graduation/interviewAi/dto/AnswerWithQuestionDto.java
@@ -3,12 +3,10 @@ import lombok.Data;
 
 @Data
 public class AnswerWithQuestionDto {
-    // Answer 관련 필드
     private int answerId;
     private String acontent;
     private int interviewId;
 
-    // Question 관련 필드
     private int questionId;
     private String qcontent;
     private int category;
