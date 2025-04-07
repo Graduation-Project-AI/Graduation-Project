7674c5a26b43ab33090589dda33685c7a2a0fd32
Jetty-Lee <jetty0188@gmail.com>
2025-04-07 11:21:20 +0900

    refactor: interviewId를 query string으로 전달하도록 수정

--- src/main/java/com/graduation/interviewAi/controller/GptQuestionController.java
+++ src/main/java/com/graduation/interviewAi/controller/GptQuestionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.graduation.interviewAi.service.GptQuestionService;
@@ -21,8 +22,7 @@ public class GptQuestionController {
     private final GptQuestionService gptQuestionService;
 
     @PostMapping("/resume-question")
-    public ResponseEntity<List<String>> generateResumeQuestions(@RequestBody Map<String, Integer> request) {
-        int interviewId = request.get("interviewId");
+    public ResponseEntity<List<String>> generateResumeQuestions(@RequestParam("interviewId") int interviewId) {
         List<String> questions = gptQuestionService.generateAndSaveResumeQuestions(interviewId);
         return ResponseEntity.ok(questions);
     }
