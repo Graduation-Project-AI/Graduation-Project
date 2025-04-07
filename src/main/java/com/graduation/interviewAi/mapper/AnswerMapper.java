24815678bf6a2789cc242e06baf2ceece8cd2999
minju00 <bin3994000@naver.com>
2025-04-07 18:01:45 +0900

    Feat: #10 stt 변환 및 db 저장

--- src/main/java/com/graduation/interviewAi/mapper/AnswerMapper.java
+++ src/main/java/com/graduation/interviewAi/mapper/AnswerMapper.java
@@ -8,5 +8,7 @@ import com.graduation.interviewAi.domain.Answer;
 @Mapper
 public interface AnswerMapper {
     void insertEmptyAnswer(@Param("questionId") int questionId, @Param("interviewId") int interviewId);
+    Answer findByInterviewIdAndQuestionId(@Param("interviewId") int interviewId, @Param("questionId") int questionId);
+    void updateAcontentByInterviewAndQuestion(@Param("interviewId") int interviewId, @Param("questionId") int questionId, @Param("acontent") String acontent);
 
 }
